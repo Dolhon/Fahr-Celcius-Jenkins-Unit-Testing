463fcad9f0c1db401c10ab3ec2b88dbc750b8503
Dolhon <sami.antila@gmail.com>
2015-04-29 04:53:36 +0700

    added use of CelcToFahr method

--- src/fahrcelcius/FahrCelcius.java
+++ src/fahrcelcius/FahrCelcius.java
@@ -24,19 +24,8 @@ public class FahrCelcius {
     
     public static void main(String[] args) {
         FahrCelcius Conv = new FahrCelcius();
-        /*Scanner in = new Scanner(System.in);
-        float temp;
-
-        System.out.println("Enter temperatue in Fahrenheit");
-        temp = in.nextInt();
-        temp = (temp - 32)*5/9;*/
         System.out.println("Celcius = " + Conv.FahrToCelc(100));
-        
-        /*System.out.println("Enter temperatue in Celcius");
-        temp = in.nextInt();
-        temp = temp * 9 / 5 + 32;
-
-        System.out.println("Fahrenheit = " + temp);
-    */}
+        System.out.println("Celcius = " + Conv.CelcToFahr(20));
+    }
     
 }
